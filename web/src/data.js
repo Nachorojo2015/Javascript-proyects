406cd40791a4dea365a5b685e10a7354d55c4e94
Ignacio Gomez <Nachorojo2015@gmail.com>
2024-07-26 17:21:31 -0300

    update-of-all-proyects

---
+++ web/src/data.js
@@ -0,0 +1,41 @@
+export const PROYECTS = [
+  {
+    name: "01-Multi-step-register-form",
+    title: "Formulario de Registro",
+    description:
+      "Pequeño formulario donde el usuario ingresa sus datos y luego son mostrados en pantalla",
+    tags: ["DOM", "Condicionales"],
+    github:
+      "https://github.com/Nachorojo2015/Javascript-proyects/tree/main/01-Multi-step-register-form",
+    web: "/01-Multi-step-register-form",
+  },
+  {
+    name: "02-Music-Player",
+    title: "Reproductor de Música",
+    description:
+      "Reproductor para escuchar música para escuchar diferentes canciones",
+    tags: ["DOM", "Manejo de etiquetas", "Eventos Click"],
+    github:
+      "https://github.com/Nachorojo2015/Javascript-proyects/tree/main/02-Music-Player",
+    web: "/02-Music-Player",
+  },
+  {
+    name: "03-Random-Quotes",
+    title: "Frases Aleatorias",
+    description: "Generador de frases de personas famosas para copiarlas y guardarlas/compartirlas",
+    tags: ["Bucles", "Manejo de data"],
+    github:
+      "https://github.com/Nachorojo2015/Javascript-proyects/tree/main/03-Random-Quotes",
+    web: "/03-Random-Quotes",
+  },
+  {
+    name: "04-Monkey-Type",
+    title: "Práctica de Mecanografía",
+    description:
+      "Programa para practicar y mejorar tus hábilidades con el teclado",
+    tags: ["Eventos del Teclado", "Manejo de data", "Manejo de clases", "DOM"],
+    github:
+      "https://github.com/Nachorojo2015/Javascript-proyects/tree/main/04-Monkey-Type",
+    web: "/04-Monkey-Type",
+  },
+];
